249ec4eb1cb479f52d446b46052bc1c719fda9c4
Volkov17 <32384533+Volkov17@users.noreply.github.com>
2018-12-23 18:37:54 +0300

    Update rsa.hpp

--- project/sourses/rsa.hpp
+++ project/sourses/rsa.hpp
@@ -1,1 +1,34 @@
 
+#ifndef BLOCKCHAIN_KURS_RSA_H
+#define BLOCKCHAIN_KURS_RSA_H
+
+
+
+#include <fstream>
+#include "cryptkey.h"
+
+using namespace std;
+
+
+class rsa
+{
+  //encrypt a string
+  hugeint static encrypt(string &data, const vector<char>  &a, const hugeint &b);
+  //decrypt a string
+  string static decrypt(hugeint &data, const vector<char>  &a, const hugeint &b);
+  //extended Euclidean algorithm
+  void static extEuclid (hugeint  a, hugeint  b, hugeint &y);
+public:
+  //get public and private keys
+  void static genKeys(cryptkey &pbkey, cryptkey &prkey);
+  //encrypt a text file
+  void static encryptTxtFile(const char *in, const char *out, cryptkey &key);
+  //decrypt a text file
+  void static decryptTxtFile(const char *in, const char*out, cryptkey &key);
+
+  string static decrypt (hugeint &data, cryptkey &key);
+
+};
+
+
+#endif 
