c1dd385f470d0148e6f71f42ddf14a31e3948033
Volkov17 <32384533+Volkov17@users.noreply.github.com>
2018-12-23 19:57:42 +0300

    Update main.cpp

--- project/main.cpp
+++ project/main.cpp
@@ -2,66 +2,11 @@
 #include <ctime>
 #include <vector>
 
-#include <unistd.h>
-#include <stdio.h>
-#include <sys/socket.h>
-#include <sys/types.h>
-#include <stdlib.h>
-#include <netinet/in.h>
-#include <string.h>
-#include <string>
-#include <arpa/inet.h>
-#include <sys/stat.h>
-
 #include "trusted_comp.h"
 #include "Block.h"
 #include "Blockchain.h"
+#include "client.h
 
-
-void connect_with_server (const char * filename){
-      int sock = socket(AF_INET, SOCK_STREAM, 0);
-      if (sock == -1) {
-            std::cerr<< "creation socket failed" <<std::endl;
-            return void();
-      }
-
-      int port = 50632;
-      std::string ipAddress = "127.0.0.1";
-
-      sockaddr_in sin;
-      sin.sin_family = AF_INET;
-      sin.sin_port = htons(port);
-      inet_pton(AF_INET, ipAddress.c_str(), &sin.sin_addr);
-
-      //	Connect to the server on the socket
-      int connectRes = connect(sock, (sockaddr*)&sin, sizeof(sin));
-      if (connectRes == -1)
-      {
-            std::cerr<< "connection failed" <<std::endl;
-            return void();
-      }
-      else {
-            std::cout << "coonsection to port : "<< port << std::endl;
-      }
-
-      if(!fork()) {
-            char sdbuf[512];
-            std::cout<<" Sending  to the Server."<<std::endl;
-            FILE *fs = fopen(filename, "r");
-            bzero(sdbuf, 512);
-            int fs_block_sz;
-            while ((fs_block_sz = fread(sdbuf, sizeof(char), 512, fs)) > 0) {
-                  if (send(sock, sdbuf, fs_block_sz, 0) < 0) {
-                        std::cout<< "failed to dens file" << std::endl;
-                        break;
-                  }
-                  bzero(sdbuf, 512);
-            }
-            fclose ( fs);
-            std::cout<< "was sent " << std::endl;
-      }
-      close(sock);
-}
 
 int main() {
 
@@ -86,7 +31,7 @@ int main() {
 
       const char * filename1 = volkovCoin.chain_to_file();
 
-      //connect_with_server(filename1);
+     
 
 
       transaction tr3 = {4 , "pasha" , "dima"};
@@ -118,6 +63,8 @@ int main() {
       }
 
       volkovCoin.checking_signature();
+      
+      connect_with_server(filename1);
 
 //      if (volkovCoin.checking_signature()){
 //          std::cout << " valid" << std::endl ;
