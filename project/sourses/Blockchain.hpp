70d2c4d08044571ccb04e4076ce7b4cef7b750c4
Volkov17 <32384533+Volkov17@users.noreply.github.com>
2018-12-23 18:31:37 +0300

    Update Blockchain.hpp

--- project/sourses/Blockchain.hpp
+++ project/sourses/Blockchain.hpp
@@ -1,1 +1,32 @@
 
+#ifndef BLOCKCHAIN_KURS_BLOCKCKAIN_H
+#define BLOCKCHAIN_KURS_BLOCKCKAIN_H
+
+#include <iostream>
+#include <vector>
+#include "Block.h"
+#include "trusted_comp.h"
+#include "rsa.h"
+
+
+
+class Blockchain {
+
+      std::vector <Block> blockchain;
+      trusted_node trust;
+
+public:
+
+    Blockchain(int gen) ;
+
+    void adding_new_block ( std::vector <transaction> &tr);
+    bool checking_signature ();
+    bool valid_chain();
+    std::string from_file_hash (const char *out);
+    void write_to_file_address(const char *in1 , hugeint a);
+    Block get_latest_block() ;
+    const char * chain_to_file ();
+    void print_chain ();
+};
+
+#endif //BLOCKCHAIN_KURS_BLOCKCKAIN_H
