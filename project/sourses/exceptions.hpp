b5815c6f31d76f770ec1cf28b4b269e9bc7ce22f
Volkov17 <32384533+Volkov17@users.noreply.github.com>
2018-12-23 18:35:00 +0300

    Update exceptions.hpp

--- project/sourses/exceptions.hpp
+++ project/sourses/exceptions.hpp
@@ -1,1 +1,34 @@
 
+#ifndef BLOCKCHAIN_KURS_EXEPTIONS_H
+#define BLOCKCHAIN_KURS_EXEPTIONS_H
+
+class mathErr
+{
+  const char *What;
+public:
+  const char *what () { return What; }
+  mathErr(const char *inp):What(inp) {}
+};
+
+
+class rsaErr
+{
+  const char *What;
+public:
+  const char *what () { return What; }
+  rsaErr(const char *inp):What(inp) {}
+};
+
+
+class commandErr {};
+
+
+class fileErr
+{
+  const char *Filename;
+public:
+  const char *filename () { return Filename; }
+  fileErr(const char *inp):Filename (inp) {}
+};
+
+#endif
