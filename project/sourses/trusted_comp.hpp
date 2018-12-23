13370d1c16d01fb70e46574dee7950a560a5384b
Volkov17 <32384533+Volkov17@users.noreply.github.com>
2018-12-23 18:33:06 +0300

    Update trusted_comp.hpp

--- project/sourses/trusted_comp.hpp
+++ project/sourses/trusted_comp.hpp
@@ -1,1 +1,36 @@
 
+#ifndef BLOCKCHAIN_KURS_TRUSTED_COMP_H
+#define BLOCKCHAIN_KURS_TRUSTED_COMP_H
+
+
+#include <iostream>
+#include <string.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <fstream>
+
+#include "Block.h"
+//#include "Blockchain.h"
+
+#include "cryptkey.h"
+#include "rsa.h"
+#include "hugeint.h"
+
+
+class trusted_node {
+
+    cryptkey privateKey;
+    cryptkey publicKey;
+
+public:
+
+    trusted_node();
+
+    void write_to_file (const char *in1 , const std::string &str_h);
+    cryptkey get_privkey();
+    cryptkey get_publkey ();
+    hugeint from_file(const char *out1);
+};
+
+
+#endif //BLOCKCHAIN_KURS_TRUSTED_COMP_H
